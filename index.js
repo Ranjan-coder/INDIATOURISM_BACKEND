d0c0804d5d3728d73fa5b953a4680845299d1890
Ranjan-coder <ranjanchanda06@gmail.com>
2024-02-17 10:53:27 +0530

    indiatourismbackend

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const {routes} = require('./Routes/routesData')
+const connectDB = require('./Config/db')
+
+require('dotenv').config()
+const port = process.env.PORT
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.use(cors({
+    origin:'*'
+}))
+
+app.use('/user',routes)
+
+
+app.listen(port,()=>{
+    connectDB()
+    console.log('Server is running');
+})
